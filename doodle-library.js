d9279fd9142989b5fc95ae2e89844f3557a59cc3
Vita <vitac215@gmail.com>
2016-10-15 15:27:02 -0400

    line, rect

--- doodle-library.js
+++ doodle-library.js
@@ -1,3 +1,6 @@
+// Things to check:
+//   text fillText(last param)
+
 /* Doodle Drawing Library
  *
  * Drawable and Primitive are base classes and have been implemented for you.
@@ -19,11 +22,15 @@ function Doodle (context) {
 Doodle.prototype.draw = function() {
  	// draw all children
     for (var i = 0; i < this.children.length; i++) {
+        console.log("children: "+this.children[i]);
         // only draw if the children are visible
         if (this.children[i].visible == true) {
+            this.context.save();
             this.children[i].draw(this.context);
+            this.context.restore();
         };
     };
+    return;
 };
 
 
@@ -103,21 +110,44 @@ function Text(attrs) {
     Drawable.call(this, attrs);
   
     //Rest of constructor code here
+    this.content = attrs.content;
+    this.fill = attrs.fill;
+    this.font = attrs.font;
+    this.size = attrs.size;
+    this.bold = attrs.bold;
 }
 Text.inheritsFrom(Drawable);
 
 //Text methods here
 Text.prototype.draw = function(context) {
- 
-}
+        // context.translate(this.left, this.top);
+        // context.rotate(this.theta);
+        // context.scale(this.scale, this.scale);
+    // apply translation ,rotation, or scale if necessary
+    applyTRS(context, this, this.left, this.top, this.theta, this.scale);
+
+    // if the text is bold
+    if (this.bold) {
+        context.font = "bold "+this.size+"pt "+this.font;
+    }
+    else {
+        context.font = this.size+"pt "+this.font;
+    }
+    context.fillStyle = this.fill; 
+    context.fillText(this.content, 0, this.getHeight());
+
+    console.log(this);  
+};
 
 Text.prototype.getWidth = function(context) {
-
-}
+    var size = MeasureText(this, this.bold, this.font, this.size);
+    return size[0];
+};
 
 Text.prototype.getHeight = function(context) {
-    
-}
+    var size = MeasureText(this, this.bold, this.font, this.size);
+    return size[1]; 
+};
 
 
 
@@ -159,21 +189,38 @@ function Line(attrs) {
     attrs = mergeWithDefault(attrs, dflt);
     Primitive.call(this, attrs);
     //Rest of constructor code here
+    this.startX = attrs.startX;
+    this.startY = attrs.startY;
+    this.endX = attrs.endX;
+    this.endY = attrs.endY;
 }
 Line.inheritsFrom(Primitive);
 
 //Line methods here
 Line.prototype.draw = function(context) {
-    
-}
+    // apply translation ,rotation, or scale if necessary
+    applyTRS(context, this, this.left, this.top, this.theta, this.scale);
+
+    context.beginPath();
+    // set the line width and line color
+    context.lineWidth = this.lineWidth;
+    context.strokeStyle = this.color;
+    // set the start and end points, and draw the line
+    context.moveTo(this.startX, this.startY);
+    context.lineTo(this.endX, this.endY);
+    context.stroke();
+    context.closePath(); 
+
+    console.log(this); 
+};
 
 Line.prototype.getWidth = function(context) {
-    
-}
+    return this.endX - this.startX;
+};
 
 Line.prototype.getHeight = function(context) {
-    
-}
+    return this.endY - this.startY;
+};
 
 
 
@@ -187,21 +234,35 @@ function Rectangle(attrs) {
     attrs = mergeWithDefault(attrs, dflt);
     Primitive.call(this, attrs);
     //Rest of constructor code here
+    this.x = attrs.x;
+    this.y = attrs.y;
+    this.width = attrs.width;
+    this.height = attrs.height;
 }
 Rectangle.inheritsFrom(Primitive);
 
 //Rectangle Methods here
 Rectangle.prototype.draw = function(context) {
-    
-}
+    // apply translation ,rotation, or scale if necessary
+    applyTRS(context, this, this.left, this.top, this.theta, this.scale);
+    // create the rectangle
+    context.rect(this.x, this.y, this.width, this.height);
+    // set line width and strokeStyle
+    context.lineWidth = this.lineWidth;
+    context.strokeStyle = this.color;
+    // draw the rectangle
+    context.stroke();
+
+    console.log(this);
+};
 
 Rectangle.prototype.getWidth = function(context) {
     
-}
+};
 
 Rectangle.prototype.getHeight = function(context) {
     
-}
+};
 
 
 
@@ -405,4 +466,15 @@ function MeasureText(text, bold, font, size)
     __measuretext_cache__[str] = size;
     
     return size;
+}
+
+/**
+ * Apply translation, rotation, or scale to an object if necessary
+ *
+ * @return: none
+ **/
+function applyTRS(context, thisObj, left, top, theta, scale) {
+    context.translate(thisObj.left, thisObj.top);
+    context.rotate(thisObj.theta);
+    context.scale(thisObj.scale, thisObj.scale);
 }
